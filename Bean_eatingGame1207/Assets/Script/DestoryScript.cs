5c312d0283217ab58e0a448f97add7c10e8fe9b4
Baitangzoe <1094551297@qq.com>
2020-12-15 09:16:27 +0800

    提交游戏项目

---
+++ Bean_eatingGame1207/Assets/Script/DestoryScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestoryScript : MonoBehaviour
+{
+    private float speed = 1.25f;
+    public static int num = 0;
+    public static bool win = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.tag == "target")
+        {
+            Destroy(this.gameObject);
+        }
+        if(other .transform .tag == "points")
+        {
+            Destroy(other.gameObject);
+            num++;
+        }
+        if (other.transform.tag == "mub"||other .transform .tag =="mua"||other .transform .tag =="muc")
+        {
+            win = true;
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            transform.Rotate(Vector3.up * 90f);//水平旋转
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            transform.Rotate(Vector3.down * 90f);//水平旋转
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            transform.Rotate(Vector3.up * 180f);//水平旋转
+        }
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        //transform.Translate(new Vector3(0, Input.GetAxis("Jump") * 0.2f, 0));//按空格键向上跳
+    }
+}
---
+++ Bean_eatingGame1207/Assets/Script/EnemyMove.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMove : MonoBehaviour
+{
+    NavMeshAgent nav;
+    public Transform[] endPoint;
+    int index = 0;
+    private float timer = 0;
+    private float timere = 0;
+    private float speeds = 0.65f;
+    private float temp = 0.0f;
+    private bool isbool = false;
+    public GameObject enemyObj;
+    // Start is called before the first frame update
+    void Start()
+    {
+        nav = GetComponent<NavMeshAgent>();
+        nav.SetDestination(endPoint[index].position);//获取目标点位置
+        temp = nav.speed;
+    }
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other .transform .tag == "Jr")
+        {
+            GameObject bzs = Instantiate(Resources.Load("bz", typeof(GameObject)) as GameObject);
+            Destroy(other.gameObject);
+            bzs.transform.SetParent(transform);
+            bzs.transform.localPosition = Vector3.zero;
+            Destroy(bzs, 1);
+            nav.speed = speeds;
+            enemyObj.SetActive(true);
+            isbool = true;
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(isbool==true)
+        {
+            timere += Time.deltaTime;
+            if (timere >= 20.0f)
+            {
+                isbool = false;
+                timere = 0f;
+                nav.speed = temp;
+                enemyObj.SetActive(false);
+            }
+        }
+        
+        if (nav.remainingDistance <= 0.01f)
+        {
+            timer += Time.deltaTime;
+            if (timer >= 0.1f)
+            {
+                timer = 0;
+                index++;
+                index %= endPoint.Length;
+                nav.SetDestination(endPoint[index].position);
+            }
+        }
+    }
+}
